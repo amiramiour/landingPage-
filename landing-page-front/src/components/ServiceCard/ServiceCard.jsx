c5fad8afb3ebae23db3a390de4d7d7e09cc8642c
amiramiour <amiouramirtahar@gmail.com>
2024-12-19 11:53:28 +0100

    serivce prestation 1

--- landing-page-front/src/components/ServiceCard/ServiceCard.jsx
+++ landing-page-front/src/components/ServiceCard/ServiceCard.jsx
@@ -4,11 +4,15 @@ import './ServiceCard.css';
 const ServiceCard = ({ title, description, icon, category, date }) => {
   return (
     <div className="service-card">
-      <img src={icon} alt={title} className="service-icon" />
+      <img src={icon} alt={title} className="service-card-image" />
+      <div className="service-qualifies">Service qualifiés</div>
       <h3>{title}</h3>
       <p>{description}</p>
       <div className="service-meta">
-        <span className="category">{category}</span>
+        <div className="category-container">
+          <img src={icon} alt={category} className="category-icon" />
+          <span className="category">{category}</span>
+        </div>
         <span className="date">{date}</span>
       </div>
     </div>
