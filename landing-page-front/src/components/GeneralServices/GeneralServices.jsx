8af0ce84fefa575bc6862fe69f48df3a740d7f32
amiramiour <amiouramirtahar@gmail.com>
2024-12-19 14:22:43 +0100

    V1 Amir

---
+++ landing-page-front/src/components/GeneralServices/GeneralServices.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import ServiceCard from '../ServiceCard/ServiceCard';
+import './GeneralServices.css';
+import receptionist from '../../assets/recept.jpeg';
+
+
+const servicesData = [
+  {
+    title: 'Réceptionniste bilingue/trilingue',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    category: 'Office de tourisme en France',
+    date: '15 DEC 2024',
+    icon: receptionist,
+  },
+  {
+    title: 'Animateur interculturel',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    category: 'CDJ',
+    date: '10 DEC 2024',
+    icon: receptionist,
+  },
+  {
+    title: 'Assistant relation client international',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    category: 'Groupe valorlty',
+    date: '07 DEC 2024',
+    icon: receptionist,
+  },
+  {
+    title: 'Assistant export',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    category: 'Groupe valorlty',
+    date: '03 DEC 2024',
+    icon: receptionist,
+  },
+];
+
+const GeneralServices = () => {
+  return (
+    <section className="services">
+      <div className="services-header-section">
+        <h2>PRESTATIONS GENERALES</h2>
+        <p className="services-description-section">
+          Pour les missions accessibles sans formation préalable ou expertise particulière.
+        </p>
+        <button className="voir-plus-section">Voir plus</button>
+      </div>
+      <div className="services-scroll-section">
+        <div className="services-grid-section">
+          {servicesData.map((service, index) => (
+            <ServiceCard key={index} {...service} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GeneralServices;
