fa5ae196bf8bb994bd305020c33b44747ef9ba9e
amiramiour <amiouramirtahar@gmail.com>
2024-12-19 21:44:30 +0100

    responsive

--- landing-page-front/src/App.jsx
+++ landing-page-front/src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import Condition from './components/Condition/Condition';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Information from './components/Information/information';
-import Services from './components/Services/Services';
-
-import GeneralServices from './components/GeneralServices/GeneralServices';
-import Footer from './components/Footer/Footer';
+import Home from './components/screens/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Hero />
-      <Services />
-      <GeneralServices />
-      <Information />
-      <Condition />
-      <Footer />
+      <Home />
     </div>
   );
 }
