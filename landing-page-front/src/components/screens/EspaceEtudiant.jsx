91fc4ae3b3c0afb968556f092ec638f6a2132714
amiramiour <amiouramirtahar@gmail.com>
2024-12-20 01:55:06 +0100

    final version

--- landing-page-front/src/components/screens/EspaceEtudiant.jsx
+++ landing-page-front/src/components/screens/EspaceEtudiant.jsx
@@ -5,21 +5,18 @@ import Header from '../Header/Header';
 import EtudHero from '../EtudHero/EtudHero';
 import Footer from '../Footer/Footer';
 import Formulaire from '../Formulaire/Formulaire';
-import Header from '../Header/Header';
-import Prestas from '../PrestatinQualifis/Prestas';
 
-function Home() {
+function EspaceEtudiant() {
   return (
     <div className="app">
       <Header />
       <EtudHero />
       <Prestas />
       <PrestasGen />
-
       <Formulaire />
       <Footer />
     </div>
   );
 }
 
-export default Home;
+export default EspaceEtudiant;
