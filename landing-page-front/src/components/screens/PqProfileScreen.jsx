1c71e5d421f49aa11ad31b608eadf180b7cc8907
amiramiour <amiouramirtahar@gmail.com>
2025-04-26 17:48:25 +0200

    screen presta qualifié

---
+++ landing-page-front/src/components/screens/PqProfileScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import PqProfile from '../PqProfile/PqProfile';
+
+function ProfileEntrepriseScreen() {
+  return (
+    <div className="app">
+      <Header />
+      <PqProfile />
+      <Footer />
+    </div>
+  );
+}
+
+export default ProfileEntrepriseScreen;
