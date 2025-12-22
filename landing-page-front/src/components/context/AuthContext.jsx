f9d81ff26485b44569439d3975979daba482163c
amiramiour <amiouramirtahar@gmail.com>
2025-12-22 13:56:53 +0100

    add auth context

--- landing-page-front/src/components/context/AuthContext.jsx
+++ landing-page-front/src/components/context/AuthContext.jsx
@@ -1,20 +1,30 @@
-import React, { createContext, useState, useContext } from 'react';
+import { createContext, useContext, useState } from "react";
 
-const AuthContext = createContext();
+const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); // null = non connecté
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
 
-  const login = (userData) => {
-    setUser(userData); // exemple: { name: "Toto", photo: "..." }
+  const login = (userData, token) => {
+    setUser(userData);
+    setToken(token);
+
+    localStorage.setItem("user", JSON.stringify(userData));
+    localStorage.setItem("token", token);
   };
 
   const logout = () => {
     setUser(null);
+    setToken(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
