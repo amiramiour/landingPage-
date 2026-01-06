a85b36f310c1a54747a80cc91ec172619aa6b4b7
amiramiour <amiouramirtahar@gmail.com>
2026-01-06 15:08:20 +0100

    a modofier HistoriqueEtudiant

---
+++ landing-page-front/src/components/ProfileEtudiant/HistoriqueEtudiant.jsx
@@ -0,0 +1,10 @@
+function HistoriqueEtudiant() {
+  return (
+    <div style={{ padding: "80px 0", textAlign: "center" }}>
+      <h2>Historique</h2>
+      <p>Contenu à définir plus tard</p>
+    </div>
+  );
+}
+
+export default HistoriqueEtudiant;
