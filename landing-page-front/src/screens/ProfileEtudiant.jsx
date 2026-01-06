71ee7edcf5886230898940f73ddcf84af72a9f18
amiramiour <amiouramirtahar@gmail.com>
2026-01-06 15:05:32 +0100

    edit ProfileEtudiantScreen

--- landing-page-front/src/screens/ProfileEtudiant.jsx
+++ landing-page-front/src/screens/ProfileEtudiant.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import ProfileEtudiant from "../components/ProfileEtudiant/ProfileEtudiant";
+import ProfileEtudiantPage from "../components/ProfileEtudiant/ProfileEtudiantPage";
 
 function ProfileEtudiantScreen() {
   return (
-    <div className="app">
+    <div className="app profile-etudiant">
       <Header />
-      <main style={{ minHeight: "80vh" }}>
-        <ProfileEtudiant />
+      <main style={{ minHeight: "80vh", paddingTop: "70px" }}>
+        <ProfileEtudiantPage />
       </main>
       <Footer />
     </div>
